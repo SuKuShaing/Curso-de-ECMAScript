2ca6244adb4c4cd46fdb9c6cf771bfc35a341e46
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-11-19 12:14:27 -0300

    forma sencilla de hacer un exponencial

---
+++ src/es7/00-exponetial.js
@@ -0,0 +1,4 @@
+// En el ECMAScript 7 se agrega el operador ** para realizar la operación de exponenciación.
+// antes se podía realizar con Math.pow(base, exponente) pero ahora es más sencillo.
+const data = 3 ** 4;
+console.log(data); // 81
